c698ea95794a7d91b31a816574cf113bfa8f7df0
Kabiir Krishna <kabiirk@gmail.com>
2020-10-08 20:42:01 +0530

    Adding upload functionality
    Everything can be done from the script now, bye-bye drag-and-drop !

--- concat.py
+++ concat.py
@@ -18,3 +18,51 @@ stack.drop(['Unnamed: 0', 'Unnamed: 0.1'], axis='columns', inplace=True)
 print(stack)
 
 st = stack.to_csv(r'C:\Users\username\..\concat.csv')
+
+#Functionality to upload files directly to drive, modify filepath as required
+# pip install pydrive
+from pydrive.drive import GoogleDrive 
+from pydrive.auth import GoogleAuth 
+   
+# For using listdir() 
+import os 
+   
+  
+# Below code does the authentication 
+# part of the code 
+gauth = GoogleAuth() 
+  
+# Creates local webserver and auto 
+# handles authentication. 
+gauth.LocalWebserverAuth()        
+drive = GoogleDrive(gauth) 
+   
+# replace the value of this variable 
+# with the absolute path of the directory 
+path = r"C:\Users\username\..\concat.csv"   
+   
+# iterating thought all the files/folder 
+# of the desired directory 
+for x in os.listdir(path): 
+   
+    f = drive.CreateFile({'title': x}) 
+    f.SetContentFile(os.path.join(path, x)) 
+    f.Upload() 
+  
+    # Due to a known bug in pydrive if we  
+    # don't empty the variable used to 
+    # upload the files to Google Drive the 
+    # file stays open in memory and causes a 
+    # memory leak, therefore preventing its  
+    # deletion 
+    f = None
+
+    
+    
+    
+    
+    
+    
+    
+    
+#credits : https://www.geeksforgeeks.org/uploading-files-on-google-drive-using-python/
